62185ed21f0e9a1f55bba58cc968b5d1faa49b6a
Sree <sreelakshmi1806200@gmail.com>
2024-08-06 22:13:22 +0530

    Add files via upload

---
+++ demo.js
@@ -0,0 +1,18 @@
+const amounts=document.querySelector(".amount");
+const btn=document.querySelector(".calculate");
+const tips=document.querySelector(".tip");
+const totals=document.querySelector(".total");
+const errors=document.querySelector(".error")
+
+function tipcalculator(){
+   const amountvalue=parseFloat(amounts.value);
+   const tipValue=parseFloat(tips.value);
+   const totalAmount=amountvalue+(amountvalue * tipValue / 100);
+   totals.innerText=totalAmount.toFixed(2);
+ 
+if(amounts.value==""||tipValue==""){
+    errors.innerHTML="please enter the correct value";
+    errors.style.display="block";
+    totals.style.display="none";
+}}
+btn.addEventListener("click",tipcalculator);
